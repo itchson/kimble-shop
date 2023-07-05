8fe9c956f21d43303382abdec735e1d6338b75da
Itchson <mitchowr@gmail.com>
2023-07-06 01:06:22 +1000

    comit

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,8 @@ import Head from 'next/head';
 
 const IndexPage = () => {
   const [isLogoHovered, setLogoHovered] = useState(false);
-
-  const linkStyle = {
-    color: isLogoHovered ? 'orange' : 'white',
-    textDecoration: 'none',
-    transition: 'color 0.3s ease',
-  };
+  const [isLink1Hovered, setLink1Hovered] = useState(false);
+  const [isLink2Hovered, setLink2Hovered] = useState(false);
 
   const handleLogoMouseEnter = () => {
     setLogoHovered(true);
@@ -18,11 +14,35 @@ const IndexPage = () => {
     setLogoHovered(false);
   };
 
+  const handleLink1MouseEnter = () => {
+    setLink1Hovered(true);
+  };
+
+  const handleLink1MouseLeave = () => {
+    setLink1Hovered(false);
+  };
+
+  const handleLink2MouseEnter = () => {
+    setLink2Hovered(true);
+  };
+
+  const handleLink2MouseLeave = () => {
+    setLink2Hovered(false);
+  };
+
+  const linkStyle = {
+    color: isLogoHovered || isLink1Hovered || isLink2Hovered ? 'orange' : 'white',
+    textDecoration: 'none',
+    transition: 'color 0.3s ease',
+  };
+
+  const currentYear = new Date().getFullYear();
+
   return (
     <div style={{ minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
       <Head>
-        <title>KIMBLE.SHOP</title>
-        <meta name="description" content="Welcome to KIMBLE.SHOP - Your source for collectibles and rare items" />
+        <title>KIMBLE.SHOP - Antiques and Collectables</title>
+        <meta name="description" content="Welcome to KIMBLE.SHOP, a family-owned eBay store based in Brisbane, Australia, specializing in antiques and collectables. We offer a diverse range of authentic collectables, including sports and Pokémon trading cards, vintage toys, rare coins & notes, board games, art pieces, and jewelry. Secure shipping, accurate descriptions, and top-notch customer service. Start your collecting journey with KIMBLE.SHOP today." />
       </Head>
       <div style={{ textAlign: 'center' }}>
         <a href="https://www.ebay.com.au/usr/kimble.shop">
@@ -34,16 +54,32 @@ const IndexPage = () => {
               height: '200px',
               transform: isLogoHovered ? 'rotate(360deg)' : 'rotate(0deg)',
               transition: 'transform 1s ease',
+              margin: '0 auto',
             }}
             onMouseEnter={handleLogoMouseEnter}
             onMouseLeave={handleLogoMouseLeave}
           />
         </a>
         <h1 style={{ marginTop: '20px' }}>
-          <a href="https://www.ebay.com.au/usr/kimble.shop" style={linkStyle}>
+          <a href="https://www.ebay.com.au/usr/kimble.shop" style={{ ...linkStyle, textDecoration: 'none' }} onMouseEnter={handleLink1MouseEnter} onMouseLeave={handleLink1MouseLeave}>
             KIMBLE.SHOP
           </a>
         </h1>
+        <p style={{ color: '#9ce1ff', maxWidth: '600px', margin: '20px auto' }}>
+          Welcome to{' '}
+          <a href="https://www.ebay.com.au/usr/kimble.shop" style={{ ...linkStyle }} onMouseEnter={handleLink2MouseEnter} onMouseLeave={handleLink2MouseLeave}>
+            KIMBLE.SHOP
+          </a>
+          , a family-owned eBay store based in Brisbane, Australia, specializing in antiques and collectables. We provide a
+ diverse range of collectables, including sports and Pokémon trading cards, vintage toys, rare coins & notes, board games, art pieces, and jewelry. Every item in our collection is authentic, thoroughly quality-checked, and a unique piece of history ready to be added to your collection. We ensure secure and swift shipping, accurate item descriptions, and prioritize top-notch customer service. Feedback from our valued customers fuels our continuous improvement. Embark on your unique collecting journey with{' '}
+          <a href="https://www.ebay.com.au/usr/kimble.shop" style={linkStyle}>
+            KIMBLE.SHOP
+          </a>{' '}
+          today.
+        </p>
+        <p style={{ color: 'gray', marginTop: '50px' }}>
+          &copy; {currentYear} KIMBLE.SHOP. All rights reserved.
+        </p>
       </div>
     </div>
   );
