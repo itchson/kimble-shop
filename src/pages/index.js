228ff106c3e26bc20d12a0beb70389043d8f9e8e
Itchson <mitchowr@gmail.com>
2023-07-05 23:06:55 +1000

    init commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const IndexPage = () => {
+  return (
+    <div style={{ background: '#1b1734', minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <div style={{ textAlign: 'center' }}>
+        <img src="/logo.png" alt="Kimble.shop Logo" style={{ width: '200px', height: '200px' }} />
+        <h1 style={{ color: 'white', marginTop: '20px' }}>KIMBLE.SHOP</h1>
+      </div>
+    </div>
+  );
+};
+
+export default IndexPage;
